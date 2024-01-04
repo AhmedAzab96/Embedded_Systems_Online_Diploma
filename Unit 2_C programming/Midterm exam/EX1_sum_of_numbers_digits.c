5502e4e9dfb73e8734a5dea1224179526fb97aad
Your Name <ahmedaz@gmail.com>
2024-01-04 03:24:13 +0200

    Adding comments

--- Unit 2_C programming/Midterm exam/EX1_sum_of_numbers_digits.c
+++ Unit 2_C programming/Midterm exam/EX1_sum_of_numbers_digits.c
@@ -1,14 +1,18 @@
 
+// A program to sum the digits of a given number
 
 #include "stdio.h"
 
+// Function to sum the numbers of the digits
 int sum_of_numbers_digits(int num)
 {
 	int sum = 0, rem = 0;
-
+	// Checking that number is not equal to zero
 	while(num != 0)
 		{
 			rem = num % 10;
+
+			// Adding the leftmost digit to the sum
 			sum += rem;
 			num /= 10;
 		}
@@ -19,12 +23,15 @@ void main()
 {
 	int number;
 
+	// Asking the user to enter a number
 	printf("Enter the number: ");
 	fflush(stdin);
 	fflush(stdout);
 
+	// Getting number from user
 	scanf("%d", &number);
 
+	// Printing output
 	printf("Input: %d -> Output: %d", number, sum_of_numbers_digits(number));
 	fflush(stdin);
 	fflush(stdout);
